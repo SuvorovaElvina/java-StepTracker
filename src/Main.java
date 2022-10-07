04bda893359bc4a31eb1d1e5ea88e9375aa41bfa
Elvina <elvina.smirnova.205@mail.ru>
2022-10-07 19:32:40 +0300

    перенесла из main и исправила лучшую серию

--- src/Main.java
+++ src/Main.java
@@ -13,31 +13,11 @@ public class Main {
         int command = scanner.nextInt();
         while (command != 0) {
             if (command == 1) {
-                System.out.println("Какой день хотите выбрать?");
-                System.out.println("Укажите месяц (цифрой):");
-
-                int month = scanner.nextInt();
-                month = this.testMonth(month)-1;
-
-                System.out.println("Укажите день:");
-                int day = scanner.nextInt();
-                day = this.testDay(day) - 1;
-
-                System.out.println("Укажите шаги:");
-                int step = scanner.nextInt();
-                step = this.testStep(step);
-
-                System.out.println("Количество шагов " + (day + 1) + "." + (month + 1) + ": " + step);
-                stepTracker.stepInDay(month, day, step);
+                stepTracker.commandOne();
             } else if (command == 2) {
-                System.out.println("За какой месяц напечатать статистику?");
-                int month = scanner.nextInt();
-                month = this.testMonth(month)-1;
-                stepTracker.statistic(month);
-
+                stepTracker.commandTwo();
             } else if (command == 3) {
-                System.out.println("Какую ежедневную цель хотите установить?");
-                System.out.println("Поздравляю, теперь ваша ежедневная цель: " + stepTracker.target());
+                stepTracker.commandThree();
             } else if (command < 0 | command > 3) {
                 System.out.println("Пожалуйста, введите правильное значение.");
             }
@@ -46,32 +26,11 @@ public class Main {
         }
         System.out.println("Программа завершена.");
     }
-    private void printMenu(){
+    public void printMenu(){
         System.out.println("Что вы желаете сделать?");
         System.out.println("1 - Ввести количество шагов за определённый день.");
         System.out.println("2 - Напечатать статистику за определённый месяц.");
         System.out.println("3 - Изменить цель по количеству шагов в день.");
         System.out.println("0 - Выход из приложения.");
-    }
-    public int testMonth(int month) {
-        while (month < 1 | month > 12) {
-            System.out.println("Прошу, проверьте вводимое значение и повторите ввод:");
-            month = scanner.nextInt();
-        }
-        return month;
-    }
-    public int testDay(int day) {
-        while (day < 1 | day > 30) {
-            System.out.println("Прошу, проверьте вводимое значение и повторите ввод:");
-            day = scanner.nextInt();
-        }
-        return day;
-    }
-    public int testStep(int step) {
-        while (step < 0) {
-            System.out.println("Пожалуйста, введите положительное значение.");
-            step = scanner.nextInt();
-        }
-        return step;
     }
 }
--- src/StepTracker.java
+++ src/StepTracker.java
@@ -10,33 +10,73 @@ public class StepTracker {
     private Scanner scanner = new Scanner(System.in);
     private Converter converter = new Converter();
 
-    public int[][] stepInDay(int month, int day, int step) {
+    public void commandOne(){
+        System.out.println("Какой день хотите выбрать?");
+        System.out.println("Укажите месяц (цифрой):");
+
+        int month = scanner.nextInt();
+        month = this.testMonth(month)-1;
+
+        System.out.println("Укажите день:");
+        int day = scanner.nextInt();
+        day = this.testDay(day) - 1;
+
+        System.out.println("Укажите шаги:");
+        int step = scanner.nextInt();
+        step = this.testStep(step);
+
+        System.out.println("Количество шагов " + (day + 1) + "." + (month + 1) + ": " + step);
+        this.stepInDay(month, day, step);
+    }
+
+    public void commandTwo(){
+        System.out.println("За какой месяц напечатать статистику?");
+        int month = scanner.nextInt();
+        month = this.testMonth(month)-1;
+        this.statistic(month);
+    }
+
+    public void commandThree(){
+        System.out.println("Какую ежедневную цель хотите установить?");
+        System.out.println("Поздравляю, теперь ваша ежедневная цель: " + this.target());
+    }
+
+    private int[][] stepInDay(int month, int day, int step) {
         year[month][day] = step;
         return year;
     }
 
-    public void statistic(int month) {
-            for (int j = 0; j < year[month].length; j++) {
-                System.out.println((j + 1) + " день: " + year[month][j]);
-            }
-            this.allStep(month);
-            this.maxStep(month);
-            this.meanStep(month);
+    private void statistic(int month) {
+        for (int j = 0; j < year[month].length; j++) {
+            System.out.println((j + 1) + " день: " + year[month][j]);
+        }
+        this.allStep(month);
+        this.maxStep(month);
+        this.meanStep(month);
 
-            System.out.println("Пройдена дистанция в " + converter.km(allStep) + " километров");
-            System.out.println("Количество сожжённых килокалорий: " + converter.kilocalories(allStep));
-            allStep = 0;
+        System.out.println("Пройдена дистанция в " + converter.km(allStep) + " километров");
+        System.out.println("Количество сожжённых килокалорий: " + converter.kilocalories(allStep));
+        allStep = 0;
 
-            this.bestSeries(month);
+        this.printBestSeries(this.bestSeries(month));
     }
 
-    private void bestSeries(int i) {
-        int days = 0;
-        for (int j = 0; j < year[i].length; j++) {
+    private int bestSeries(int i) {
+        int currentSeries = 0, bestSeries = 0;
+        for (int j = 0; j < year[i].length - 1; j++) {
             if (year[i][j] >= this.purpose) {
-                days++;
+                currentSeries++;
+                if (currentSeries > bestSeries) {
+                    bestSeries = currentSeries;
+                }
+            } else {
+                currentSeries = 0;
             }
         }
+        return bestSeries;
+    }
+
+    private void printBestSeries(int days) {
         String dayStr = "дней";
         if (days == 1 || days == 21) {
             dayStr = "день";
@@ -71,14 +111,37 @@ public class StepTracker {
         System.out.println("Общее количество шагов: " + allStep);
     }
 
-    public int target() {
+    private int target() {
         Scanner scanner = new Scanner(System.in);
-        int purpose;
         purpose = scanner.nextInt();
         while (purpose < 0) {
             System.out.println("Пожалуйста, вводите положительное значение: ");
             purpose = scanner.nextInt();
         }
         return purpose;
+    }
+
+    private int testMonth(int month) {
+        while (month < 1 | month > 12) {
+            System.out.println("Прошу, проверьте вводимое значение и повторите ввод:");
+            month = scanner.nextInt();
+        }
+        return month;
+    }
+
+    private int testDay(int day) {
+        while (day < 1 | day > 30) {
+            System.out.println("Прошу, проверьте вводимое значение и повторите ввод:");
+            day = scanner.nextInt();
+        }
+        return day;
+    }
+
+    private int testStep(int step) {
+        while (step < 0) {
+            System.out.println("Пожалуйста, введите положительное значение.");
+            step = scanner.nextInt();
+        }
+        return step;
     }
 }
